f5b5a5a15c667815eb7c82d5539f56dff0b9cfa5
Wojciech Dlugosz <webmaster@styronet.pl>
2024-09-06 16:12:47 +0200

    Add tests to function convertPLNtoUSD

--- src/utils/convertPLNToUSD.js
+++ src/utils/convertPLNToUSD.js
@@ -1,7 +1,15 @@
 export const convertPLNToUSD = (PLN) => {
 
+  if (typeof PLN === 'string' || PLN === undefined) {
+    return NaN;
+  } else if (PLN < 0) {
+    return '$0.00';
+  } else if (typeof PLN !== 'string' && typeof PLN !== 'number') {
+    return 'Error';
+  }
+
   const PLNtoUSD = PLN / 3.5;
-  
+
   const formatter = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: 'USD'
