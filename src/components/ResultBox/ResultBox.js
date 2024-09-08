f5acaad2d7e9832ae84849093e5843e971bef164
Wojciech Dlugosz <webmaster@styronet.pl>
2024-09-08 14:08:22 +0200

    Add integration tests for ResultBox

--- src/components/ResultBox/ResultBox.js
+++ src/components/ResultBox/ResultBox.js
@@ -15,11 +15,19 @@ const ResultBox = ({ from, to, amount }) => {
 
   const formattedAmount = useMemo(() => formatAmountInCurrency(amount, from), [amount, from]);
 
-  return (
-    <div data-testid='output' className={styles.result}>
-      {formattedAmount} = {convertedAmount}
-    </div>
-  );
+  if (amount < 0) {
+    return (
+      <div data-testid='wrong' className={styles.result}>
+        <p>Wrong amount</p>
+      </div>
+    );
+  } else {
+    return (
+      <div data-testid='output' className={styles.result}>
+        {formattedAmount} = {convertedAmount}
+      </div>
+    );  
+  }
 };
 
 ResultBox.propTypes = {
--- src/components/ResultBox/ResultBox.test.js
+++ src/components/ResultBox/ResultBox.test.js
@@ -33,4 +33,82 @@ describe("Component ResultBox", () => {
       cleanup();
     }
   });
+  it('should render proper info about conversion when USD -> PLN', () => {
+
+    const testCases = [
+      { from: 'USD', to: 'PLN', amount: 100 },
+      { from: 'USD', to: 'PLN', amount: 30 },
+      { from: 'USD', to: 'PLN', amount: 220 },
+      { from: 'USD', to: 'PLN', amount: 1537 },
+    ];
+
+    for (const testObj of testCases) {
+
+      const amount = (testObj.amount).toFixed(2);
+      const from = testObj.from;
+      const to = testObj.to;
+
+      render(<ResultBox from={from} to={to} amount={Number(amount)} />);
+
+      const output = screen.getByTestId('output');
+
+      const result = (amount*3.5).toFixed(2).replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+      expect(output).toHaveTextContent(`$${amount.replace(/\B(?=(\d{3})+(?!\d))/g, ',')} = PLN ${result}`);
+
+      cleanup();
+    }
+  });
+
+  it('should render proper info about conversion when PLN -> PLN', () => {
+
+    const testCases = [
+      { from: 'PLN', to: 'PLN', amount: 100 },
+      { from: 'PLN', to: 'PLN', amount: 30 },
+      { from: 'PLN', to: 'PLN', amount: 220 },
+      { from: 'PLN', to: 'PLN', amount: 1537 },
+    ];
+
+    for (const testObj of testCases) {
+
+      const amount = (testObj.amount).toFixed(2);
+      const from = testObj.from;
+      const to = testObj.to;
+
+      render(<ResultBox from={from} to={to} amount={Number(amount)} />);
+
+      const output = screen.getByTestId('output');
+
+      const formatedAmount = amount.replace(/\B(?=(\d{3})+(?!\d))/g, ',');
+
+      expect(output).toHaveTextContent(`PLN ${formatedAmount} = PLN ${formatedAmount}`);
+
+      cleanup();
+    }
+  });
+
+  it('should render error when amount < 0', () => {
+
+    const testCases = [
+      { from: 'PLN', to: 'USD', amount: '-100' },
+      { from: 'PLN', to: 'USD', amount: '-30' },
+      { from: 'PLN', to: 'USD', amount: '-220' },
+      { from: 'PLN', to: 'USD', amount: '-1537' },
+    ];
+
+    for (const testObj of testCases) {
+
+      const amount = (testObj.amount);
+      const from = testObj.from;
+      const to = testObj.to;
+
+      render(<ResultBox from={from} to={to} amount={Number(amount)} />);
+
+      const output = screen.getByTestId('wrong');
+
+      expect(output).toHaveTextContent(`Wrong amount`);
+
+      cleanup();
+    }
+  });
 });
